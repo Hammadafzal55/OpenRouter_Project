f9ff72e3fb8eba1bbdf5d2e53d5efaa1cabfb07b
Hammad Afzal <hammadafzalb235@gmail.com>
2025-06-03 15:20:53 +0500

    main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def get_response(model_id, prompt):
     body = {
         "model": model_id,
         "messages": [{ "role": "user","content": prompt }],
-        "max_tokens": max_tokens,
+        "max_tokens": 1500,
     }
 
     res = requests.post(url, headers=headers, data=json.dumps(body))
